5749e7e2d53cb955287f47086a255bb97539225a
diniapsari97 <apsaridini97@gmail.com>
2020-02-23 12:16:01 +0700

    bilangan prima

---
+++ bilanganprima/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bilanganprima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bilangan;
+            Console.Write("masukkan batas bilangan :");
+            bilangan = Convert.ToInt32(Console.ReadLine());
+            int temp = 0;
+            Console.WriteLine();
+            Console.WriteLine("bilangan prima 1 sampai {0} adalah ", bilangan);
+            for (int i=1; i<=bilangan; i++)
+            {
+                for (int j=1; j<=i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        temp += 1;
+                    }
+                }
+                if (temp==2)
+                {
+                    Console.Write(i+" ");
+                }
+                temp = 0;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
